9492caa00d770d345f3e87e3f10c79142be8db04
ogawakeisuke <ogawa@uniba.jp>
2013-07-24 00:02:57 +0900

    もしかしたらfft関数見直し必要かも

--- audio_fft.rb
+++ audio_fft.rb
@@ -1,6 +1,7 @@
 require "ruby-audio"
 require "narray"
 require "fftw3"
+require 'cairo'
 
 
 fname = "dot.wav"
@@ -35,6 +36,7 @@ RubyAudio::Sound.open(fname) do |snd|
     # fftという配列にfftの結果をぶち込む
     # fft配列は512個、ここに時間ベースで値が入って行く！
     # つまりfft[]が周波数、そこのインデックスが時間ベース
+    # うん？やっぱ違うかも
     j = 0
     fft_slice.each do |x| 
       fft[j] << x 
@@ -53,15 +55,30 @@ end
 
 
 def color_pick_print(value)
-  if value < 100
+  if value < 20
     "."
-  elsif value > 100 && value < 180
-    ":"
+  elsif value > 20 && value < 150
+    "*"
   else
     "#" 
   end
 end
 
+def ret_color_rows(compleces)
+  ret_array = []
+  compleces.each do |complex|
+    ret_array << color_pick_print(amp(complex).scale_between)
+  end
+  ret_array
+end
+
+def ret_scale_rows(compleces)
+  ret_array = []
+  compleces.each do |complex|
+    ret_array << amp(complex).scale_between
+  end
+  ret_array
+end
 
 
 #
@@ -69,16 +86,42 @@ end
 #
 class Numeric
   def scale_between(from_min = 0.0, from_max = 1.0, to_min = 0, to_max = 255)
-    ((to_max - to_min) * (self - from_min)) / (from_max - from_min) + to_min
+   ( ((to_max - to_min) * (self - from_min)) / (from_max - from_min) + to_min ).round  # + 1
   end
 end
 
 
+#
+# 描画空間
+#
+format = Cairo::FORMAT_ARGB32
+width = 3000
+height = 600
+radius = 3 # 半径
 
-fft.each do |compleces|
-  compleces.each do |complex|
-    p color_pick_print( amp(complex).scale_between )
+surface = Cairo::ImageSurface.new(format, width, height)
+context = Cairo::Context.new(surface)
+
+# 背景
+context.set_source_rgb(1, 1, 1) # 白
+context.rectangle(0, 0, width, height)
+context.fill
+
+# 赤丸
+
+
+
+fft.each_with_index do |compleces, i|
+  ret_scale_rows(compleces).each_with_index do |val, j|
+    context.set_source_rgb(val, 0, 0)
+    context.arc(j, i, radius, 0, 1 * Math::PI)
+    context.fill
   end
+  p ret_scale_rows(compleces)[299]
+  #break #ためしにひとつでbreak
 end
+
+surface.write_to_png("hinomaru.png")
+
 
 
