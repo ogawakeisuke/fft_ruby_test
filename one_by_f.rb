202287584fa6ab3d9779442610322d1731f9eafe
ogawakeisuke <ogawa@uniba.jp>
2013-07-26 13:43:13 +0900

    いくつか検証

--- one_by_f.rb
+++ one_by_f.rb
@@ -5,7 +5,7 @@ require './regression_line'
 
 
 
-fname = "src/pop.wav"
+fname = "pinknoise.aif"
 window_size = 1024
 results = Array.new
 
@@ -16,7 +16,7 @@ RubyAudio::Sound.open(fname) do |snd|
   while snd.read(buf) != 0
     na = NArray.to_na(buf.to_a)
 
-    fft_slice = FFTW3.fft(na, -1).to_a[0, window_size / 2].collect { |complex| complex.abs / (window_size / 2) }
+    fft_slice = FFTW3.fft(na, -1).to_a[0, window_size / 2].collect { |complex| complex.abs * 10 / (window_size / 2) }
     
     results << RegLine.new(fft_slice).regression_slope
   end
@@ -25,4 +25,8 @@ end
 
 p results
 
-p (results.inject {|sum,x| sum += x} / results.size)
+p (results.inject { |sum, x| sum += x } / results.size)
+
+
+
+# -1.0に近づけば高評価
